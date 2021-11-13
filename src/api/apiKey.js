e169de4324dc98e4bb415761c6a5da2960a479b9
Ray Li <k.r.li07717@gmail.com>
2021-11-13 14:49:45 +0800

    refactor: remove private apiKey

---
+++ src/api/apiKey.js
@@ -0,0 +1,8 @@
+// 需向TDX API申請api key: https://tdx.transportdata.tw/
+
+const API_KEY = {
+  APP_ID: "FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF",
+  APP_KEY: "FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF",
+};
+
+export default API_KEY;
