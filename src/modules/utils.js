a0d156d12b10115874c6938f8be7314a78b16ed3
Ray Li <k.r.li07717@gmail.com>
2021-11-13 13:15:27 +0800

    refactor: refactor utils

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -9,13 +9,20 @@ function scrollTop() {
 function parseRouter(searchParamString) {
   const searchParams = new URLSearchParams(decodeURI(searchParamString));
   return [...searchParams].reduce((routerObj, [paramKey, paramValue]) => {
-    if (paramKey === "categories") {
-      routerObj[paramKey] = paramValue.split(",");
-    } else {
-      routerObj[paramKey] = paramValue;
-    }
+    paramKey === "categories"
+      ? (routerObj[paramKey] = paramValue.split(","))
+      : (routerObj[paramKey] = paramValue);
     return routerObj;
   }, {});
 }
 
-export { scrollTop, parseRouter };
+function isInvalidQuery() {
+  const onloadSearchParams = new URLSearchParams(location.search);
+  return (
+    !onloadSearchParams.has("city") &&
+    !onloadSearchParams.has("categories") &&
+    !onloadSearchParams.has("keyword")
+  );
+}
+
+export { scrollTop, parseRouter, isInvalidQuery };
